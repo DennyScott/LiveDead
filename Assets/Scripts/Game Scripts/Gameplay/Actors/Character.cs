1908c95923189cdf457b5d3f7cdbad5d0a93400b
Denny Scott <dennyscott301@gmail.com>
2015-04-01 20:42:03 -0500

    Added Base Classes

---
+++ Assets/Scripts/Game Scripts/Gameplay/Actors/Character.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Character : Grunt {
+
+    public int health;
+    public int speed;
+    public int gcd;
+
+    public delegate void characterAction(GameObject g);
+    public event characterAction onAttack;
+    public event characterAction onDamage;
+
+
+    #region Event Triggers
+    private void TriggerOnAttack () {
+        if(onAttack != null) {
+            onAttack(gameObject);
+        }
+    }
+
+    private void TriggerOnDamage () {
+        if(onDamage != null) {
+            onDamage(gameObject);
+        }
+    }
+    #endregion
+
+    public void TakeDamage (int amount) {
+        health -= amount;
+        TriggerOnDamage();
+    }
+}
---
+++ Assets/Scripts/Game Scripts/Gameplay/Actors/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+    public class Player : Character {
+
+        void Start () {
+            StartCoroutine(Move());
+        }
+
+        IEnumerator Move() {
+            if(Input.GetAxis("Vertical") > 0) {
+                Debug.Log("Moving");
+            }
+            yield return null;
+        }
+    }
