9751f98c7324e096e1a9cf864e4c4e905758e4f0
Denny Scott <dennyscott301@gmail.com>
2015-04-05 00:02:53 -0400

    Cleaned up Code using Resharper

--- Assets/Scripts/Game Scripts/Gameplay/Actors/Character.cs
+++ Assets/Scripts/Game Scripts/Gameplay/Actors/Character.cs
@@ -1,29 +1,123 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public abstract class Character : Grunt {
 
-    public int health;
-    public float speed = 5.0f;
-    public int gcd;
+	#region Public Variables
+	public int Health;
+    public float Speed = 5.0f;
+    public int Gcd;
+	public enum CharacterStates { Neutral = 0, Attack = 1 };
+	protected Character Target;
+
+	//The two below values will be determined by the weapon later
+	public float AttackRange = 1.0f;
+	public float AttackView = 50.0f;
+	public float Hitbox = 1.0f;
+	#endregion
+
+	#region Private Variables
+	private CharacterStates _currentState;
+	private readonly ICharacterState[] _characterState = new ICharacterState[System.Enum.GetNames(typeof(CharacterStates)).Length];
+	#endregion
+
+	#region Monobehaviour Methods
+
+	protected virtual void Start() {
+		//Store State for later use.
+		_characterState[(int)CharacterStates.Neutral] = new NeutralState(this);
+		_characterState[(int)CharacterStates.Attack] = new AttackState(this);
+		CurrentState = CharacterStates.Neutral;
+	}
+
+	protected virtual void Update() {
+		TriggerStateUpdate();
+	}
+
+	#endregion
+
+	#region Properties
+	public CharacterStates CurrentState {
+		get {
+			return _currentState;
+		}
+		set {
+			TriggerStateExit();
+			_currentState = value;
+			TriggerStateChange();
+			TriggerStateEnter();
+			TriggerStateStart();
+		}
+	}
+	#endregion
+
+	#region Delegates
+	public System.Action<GameObject> OnDamage;
+	public System.Action<GameObject> OnStateChange;
+	public System.Action<GameObject> OnStateExit;
+	public System.Action<GameObject> OnStateEnter;
+	public System.Action<GameObject> OnStateStart;
+	public System.Action<GameObject> OnStateUpdate;
+	#endregion
 
 
 
-    public delegate void characterAction(GameObject g);
-    public event characterAction onDamage;
-
-   
-
-    #region Event Triggers
-    private void TriggerOnDamage () {
-        if(onDamage != null) {
-            onDamage(gameObject);
+	#region Event Triggers
+	void TriggerOnDamage () {
+        if(OnDamage != null) {
+            OnDamage(gameObject);
         }
     }
+
+	void TriggerStateChange() {
+		if(OnStateChange != null) {
+			OnStateChange(gameObject);
+		}
+	}
+
+	void TriggerStateEnter() {
+			OnStateEnter += _characterState[(int)CurrentState].OnStateEnter;
+			OnStateExit += _characterState[(int)CurrentState].OnStateExit;
+			OnStateStart += _characterState[(int)CurrentState].OnStart;
+			OnStateUpdate += _characterState[(int)CurrentState].OnUpdate;
+			OnStateEnter(gameObject);	
+	}
+
+	void TriggerStateStart() {
+		if(OnStateStart != null) {
+			OnStateStart(gameObject);
+		}
+	}
+
+	void TriggerStateUpdate() {
+		if(OnStateUpdate != null) {
+			OnStateUpdate(gameObject);
+		}
+	}
+
+	void TriggerStateExit() {
+		if (OnStateExit == null) {
+			return;
+		}
+		OnStateExit(gameObject);
+		OnStateEnter -= _characterState[(int)CurrentState].OnStateEnter;
+		OnStateExit -= _characterState[(int)CurrentState].OnStateExit;
+		OnStateStart -= _characterState[(int)CurrentState].OnStart;
+		OnStateUpdate -= _characterState[(int)CurrentState].OnUpdate;
+	}
     #endregion
 
-    public void TakeDamage (int amount) {
-        health -= amount;
+	#region Public Methods
+	public bool IsTargetInRange(float range) {
+		return Vector3.Distance(gameObject.transform.position, Target.transform.position) <= range + Target.Hitbox;
+	}
+
+	public bool IsTargetInView(float allowableAngle) {
+		return Vector3.Angle(transform.forward, Target.transform.position - transform.position) < allowableAngle;
+	}
+
+	public void TakeDamage (int amount) {
+        Health -= amount;
         TriggerOnDamage();
-    }
+	}
+	#endregion
 }
--- Assets/Scripts/Game Scripts/Gameplay/Actors/Player.cs
+++ Assets/Scripts/Game Scripts/Gameplay/Actors/Player.cs
@@ -1,44 +1,43 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
 
-    public class Player : Character {
+public class Player : Character {
+	public GameObject Enemy;
+	private Character _tempTarget;
 
-        private Attack attackGrunt;
-        public GameObject enemy;
-        private Attack tempTarget;
+	protected override void Start() {
+		base.Start();
+		_tempTarget = Enemy.GetComponent<Character>();
+	}
 
-        void Start() {
-            attackGrunt = gameObject.GetComponent<Attack>();
-            tempTarget = enemy.GetComponent<Attack>();
-        }
+	protected override void Update() {
+		base.Update();
+		var currentSpeed = GetCurrentSpeed();
 
-        void Update() {
-            //base.Update();
-            float currentSpeed = GetCurrentSpeed();
+		if(Input.GetButton("Fire2")) {
+			Debug.Log("Setting Attack");
+			CurrentState = CharacterStates.Attack;
+			Target = _tempTarget;
+		}
 
-            if(Input.GetButton("Fire2")) {
-                attackGrunt.AttackTarget(tempTarget);
-            }
+		var translation = Input.GetAxis("Vertical") * currentSpeed;
+		var rotation = Input.GetAxis("Horizontal") * currentSpeed;
 
-            float translation = Input.GetAxis("Vertical") * currentSpeed;
-            float rotation = Input.GetAxis("Horizontal") * currentSpeed;
-            
-            translation *= Time.deltaTime;
-            rotation *= Time.deltaTime;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
 
-            transform.Translate(0, translation, 0);
-            transform.Translate(rotation, 0, 0);
-        }
+		transform.Translate(0, translation, 0);
+		transform.Translate(rotation, 0, 0);
+	}
 
-        /// <summary>
-        /// If a player is running at both angles, don't speed the character
-        /// up by double the speed. Instead, we allow them to go at 1.5 times the pace.
-        /// 
-        /// If only one is down, return using the regular speed calculation.
-        /// </summary>
-        /// <returns>float speed to move by</returns>
-        float GetCurrentSpeed() {
-            return Input.GetAxis("Vertical") != 0 && Input.GetAxis("Horizontal") != 0 ? speed / 1.5f : speed;
-        }
-    }
+	/// <summary>
+	///     If a player is running at both angles, don't speed the character
+	///     up by double the speed. Instead, we allow them to go at 1.5 times the pace.
+	///     If only one is down, return using the regular speed calculation.
+	/// </summary>
+	/// <returns>float speed to move by</returns>
+	private float GetCurrentSpeed() {
+		const int tolerance = 0;
+		return Math.Abs(Input.GetAxis("Vertical")) > tolerance && Math.Abs(Input.GetAxis("Horizontal") - tolerance) > tolerance ? Speed / 1.5f : Speed;
+	}
+}
